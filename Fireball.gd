e89276f8d2d23afe38cf23446bab14a7db80f950
Samuel Sarette <samuelsarette@linux.com>
2019-09-21 22:40:48 -0400

    connect more signals

--- Fireball.gd
+++ Fireball.gd
@@ -4,12 +4,12 @@ onready var timer = get_node("Timer")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	visible = false
 
-func _on_Player_interact_with(name):
-	if name == get_name():
-		timer.start()
-
+func _on_StoryExample_witch_throw_fireball():
+	visible = true
+	# TODO move toward Bob
+	timer.start()
 
 func _on_Timer_timeout():
 	visible = false
---
+++ Witch.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_StoryExample_witch_enter_attacking_bob():
+	pass # Replace with function body.
+
+
+func _on_StoryExample_witch_enter_laughing():
+	pass # Replace with function body.
+
+
+func _on_StoryExample_witch_throw_fireball():
+	pass # Replace with function body.
