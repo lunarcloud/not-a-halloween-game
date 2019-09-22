5d17ae4811dc47ecf852ede090a0e8ba57c390dc
Samuel Sarette <samuelsarette@linux.com>
2019-09-22 08:19:45 -0400

    added itch upload script

---
+++ upload-itch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+butler push --fix-permissions export/windows/ "samsarette/not-a-halloween-game:win"
+butler push --fix-permissions export/macos/not-a-halloween-game.zip "samsarette/not-a-halloween-game:mac"
+butler push --fix-permissions export/linux/ "samsarette/not-a-halloween-game:linux"
