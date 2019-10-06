edbbce4b80ea55e58cc662eb2a615bcc437f3ad8
Samuel Sarette <samuelsarette@linux.com>
2019-10-06 15:53:02 -0400

    added web export

--- upload-itch.sh
+++ upload-itch.sh
@@ -4,3 +4,4 @@
 butler push --fix-permissions export/windows/ "samsarette/not-a-halloween-game:win"
 butler push --fix-permissions export/macos/not-a-halloween-game.zip "samsarette/not-a-halloween-game:mac"
 butler push --fix-permissions export/linux/ "samsarette/not-a-halloween-game:linux"
+butler push --fix-permissions export/web/ "samsarette/not-a-halloween-game:web"
