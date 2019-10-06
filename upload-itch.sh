1db5a6b6d4c60c4a511633098ddd4ec1ef800eab
Samuel Sarette <samuelsarette@linux.com>
2019-10-06 18:06:17 -0400

    Added android to itch upload script

--- upload-itch.sh
+++ upload-itch.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-
 butler push --fix-permissions export/windows/ "samsarette/not-a-halloween-game:win"
 butler push --fix-permissions export/macos/not-a-halloween-game.zip "samsarette/not-a-halloween-game:mac"
 butler push --fix-permissions export/linux/ "samsarette/not-a-halloween-game:linux"
 butler push --fix-permissions export/web/ "samsarette/not-a-halloween-game:web"
-# butler push --fix-permissions export/android/not-a-halloween-game.apk "samsarette/not-a-halloween-game:android"
+butler push --fix-permissions export/android/not-a-halloween-game.apk "samsarette/not-a-halloween-game:android"
