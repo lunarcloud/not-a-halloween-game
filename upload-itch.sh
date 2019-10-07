5a07c7511f83c86fc01652a9eb603e3b66c1ffaf
Samuel Sarette <samuelsarette@linux.com>
2019-10-06 22:25:02 -0400

    update version

--- upload-itch.sh
+++ upload-itch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-butler push --fix-permissions export/windows/ "samsarette/not-a-halloween-game:win"
-butler push --fix-permissions export/macos/not-a-halloween-game.zip "samsarette/not-a-halloween-game:mac"
-butler push --fix-permissions export/linux/ "samsarette/not-a-halloween-game:linux"
-butler push --fix-permissions export/web/ "samsarette/not-a-halloween-game:web"
-butler push --fix-permissions export/android/not-a-halloween-game.apk "samsarette/not-a-halloween-game:android"
+butler -v push export/windows/ "samsarette/not-a-halloween-game:win"
+butler -v push --fix-permissions export/macos/not-a-halloween-game.zip "samsarette/not-a-halloween-game:mac"
+butler -v push --fix-permissions export/linux/ "samsarette/not-a-halloween-game:linux"
+butler -v push --fix-permissions export/web/ "samsarette/not-a-halloween-game:web"
+butler -v push export/android/not-a-halloween-game.apk "samsarette/not-a-halloween-game:android"
